d089fc8ba55689554ed39c49e0128b0988626681
LazyJazz <zijian.lv@hotmail.com>
2025-07-10 15:28:49 +0800

    Clean up unused code

--- shaders/raytracing/callable.hlsl
+++ shaders/raytracing/callable.hlsl
@@ -1,5 +1,5 @@
 #include "common.hlsli"
 
-    [shader("callable")] void Main(inout ShadingColor payload) {
+[shader("callable")] void Main(inout ShadingColor payload) {
   payload.color = payload.color * float3(0.6, 0.7, 0.8);
 }
